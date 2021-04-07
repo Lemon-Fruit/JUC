d6ecf5f73e521060b57baeaa54553ae93728d677
lemon <1240561373@qq.com>
2021-04-07 19:41:24 +0800

    interrupt & interrupted & isInterrupted的学习理解

--- src/main/java/com/swpu/interrupted/interruptTest.java
+++ src/main/java/com/swpu/interrupted/interruptTest.java
@@ -1,6 +1,6 @@
 package com.swpu.interrupted;
 
-public class interruptedTest {
+public class interruptTest {
     public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(() -> {
             while (!Thread.currentThread().isInterrupted())
@@ -11,7 +11,7 @@ public class interruptedTest {
 
         Thread.sleep(1000);
 
-        System.out.println("main thread interupt thread");
+        System.out.println("main thread interrupt thread");
         thread.interrupt();
 
         thread.join();
---
+++ src/main/java/com/swpu/interrupted/interrupted.java
@@ -0,0 +1,42 @@
+package com.swpu.interrupted;
+
+public class interrupted {
+    public static void main(String[] args) throws InterruptedException {
+        Thread threadOne = new Thread(() -> {
+            for (; ; ) {
+            }
+        });
+
+        threadOne.start();
+
+        //设置中断标志
+        threadOne.interrupt();
+
+        //获取中断标志
+        System.out.println("isInterrupted:" + threadOne.isInterrupted());
+
+        //获取中断标志，如果为true 重置标志
+        System.out.println("isInterrupted:" + threadOne.interrupted());
+        /*
+        threadOne.interrupted()等价于Thread.interrupted()
+        interrupted()为static方法
+         */
+
+        //获取中断标志，如果为true 重置标志
+        System.out.println("isInterrupted:" + Thread.interrupted());
+
+        //获取中断标志
+        System.out.println("isInterrupted:" + threadOne.isInterrupted());
+
+        threadOne.join();
+
+        System.out.println("main thread is over");
+    }
+    /*
+    result:
+    isInterrupted:true
+    isInterrupted:false
+    isInterrupted:false
+    isInterrupted:true
+     */
+}
